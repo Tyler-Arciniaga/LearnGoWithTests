344f9a98b631afa3fc65e85b91e7ae9e6ea8e80e
Tyler Arciniaga <tylerarc@umich.edu>
2025-05-24 19:24:29 -0400

    HelloWorld: Refactor #1

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func Hello(name string) string {
+	if name == "" {
+		return "Hello World"
+	}
 	return "Hello " + name
 }
 
--- HelloWorld/hello_test.go
+++ HelloWorld/hello_test.go
@@ -3,9 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	t.Run("saying hello to a specific person", func(t *testing.T) {
+		got := Hello("Tyler")
+		want := "Hello Tyler"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello to the world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 	got := Hello("Tyler")
 	want := "Hello Tyler"
+	assertCorrectMessage(t, got, want)
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
